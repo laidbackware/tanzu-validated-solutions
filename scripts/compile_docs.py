3cdd12609023cff434da5b34ae92314b7e339cb3
Matt Proud <proudmatt@gmail.com>
2022-07-19 14:21:12 +0100

    added figure numbering fix duplication issue

--- scripts/compile_docs.py
+++ scripts/compile_docs.py
@@ -1,5 +1,6 @@
 import json
 import os
+import re
 import sys
 from glob import glob
 from pprint import pprint
@@ -13,15 +14,16 @@ def main():
   for file_name in glob(manifest_dir):
     manifest = decode_json(file_name)
     ref_arch_name = os.path.basename(file_name)[:-5]
-    rendered_markdown = ""
+    rendered_sections = []
     
     for section in manifest:
       with open("{}/{}.md".format(component_dir, section), "r") as section_file:
-        rendered_markdown += section_file.read().rstrip("\n") + "\n\n"
+        rendered_sections.append(section_file.read().rstrip("\n") + "\n\n")
     
+    rendered_markdown = number_figures("".join(rendered_sections))
+
     with open("{}/{}.md".format(ref_arch_dir, ref_arch_name), 'w') as output_file:
-      for section in manifest:
-        output_file.write(rendered_markdown)    
+      output_file.write(rendered_markdown)    
 
 def decode_json(file_name):
   with open(file_name, "r") as file:
@@ -30,6 +32,20 @@ def decode_json(file_name):
     except Exception as exc:
       print("Json decoding failed with error " + str(exc), file=sys.stderr)
       raise SystemExit
+
+def number_figures(rendered_markdown):
+  '''Replace figure numbers to be incremental'''
+  split_lines = rendered_markdown.splitlines()
+  figure_number = 1
+  for line_number, line_content in enumerate(split_lines):
+    if "![Figure" in line_content:
+      split_lines[line_number] = re.sub(r"Figure [0-9]+", f"Figure {str(figure_number).zfill(2)}", line_content)
+      # Apply same figure number to any Figure footers
+      if (line_number + 2) < len(split_lines) and re.match(r"^(?!!\[).*Figure [0-9]+", split_lines[line_number + 2]):
+        split_lines[line_number + 2] = re.sub(r"Figure [0-9]+", f"Figure {str(figure_number).zfill(2)}",
+                                              split_lines[line_number + 2])
+      figure_number += 1
+  return"\n".join(split_lines)
 
 if __name__ == "__main__":
   main()
--- scripts/compile_docs_test.py
+++ scripts/compile_docs_test.py
@@ -9,3 +9,10 @@ def test_e2e():
 def test_decode_json():
   manifest = compile_docs.decode_json(base_dir + "/src/build-manifests/tko-on-vsphere.json")
   assert manifest
+
+def test_number_figures():
+  '''Ensure figures are re-numbered'''
+  markdown = "#heading\n\n\n![Figure 1 ]\n\ntest\n![Figure 05 ]\n\nFigure 05"
+  returned_markdown = compile_docs.number_figures(markdown)
+  expected = "#heading\n\n\n![Figure 01 ]\n\ntest\n![Figure 02 ]\n\nFigure 02"
+  assert returned_markdown ==  expected
